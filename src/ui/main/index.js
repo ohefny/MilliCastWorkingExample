05367407b0c31c3c3759c6cb1e40aeaa569c2fc9
Omar El Hefny <o.hefny@gmail.com>
2020-06-23 10:40:22 +0200

    feat: add publisher and subscriber

--- src/ui/main/index.js
+++ src/ui/main/index.js
@@ -4,32 +4,36 @@
 /* eslint-disable comma-spacing */
 /* eslint-disable key-spacing */
 /* eslint-disable no-unused-vars */
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 
 import { viewerRenderer } from '../viewer'
 import { broadcastRenderer } from '../publish'
 import { stateRenderer } from '../../render/state'
-import * as config from '../../config'
+import * as staticConfig from '../../config'
 import {
   mediaDevices // eslint-disable-line import/named
 } from 'react-native-webrtc'
 
-import { Text, View, TouchableHighlight, TextInput,Alert } from 'react-native'
+import { Text, View, TouchableHighlight, TextInput, Alert } from 'react-native'
 import axios from 'axios'
 
 import { styles } from './styles'
 export class Main extends Component {
   // const renderViewer = viewerRenderer(config)
-  state={ streamAccountId: config.streamAccountId, streamName: config.viewerStreamId }
+  state = {
+    streamAccountId: staticConfig.streamAccountId,
+    streamName: staticConfig.viewerStreamId,
+    publisherToken:staticConfig.publisherToken
+  };
   render () {
     const renderer = this.state.renderer
     if (renderer) return renderer()
 
     const buttonDisabled = this.state.loading
 
-    const publishPressed = async () => {
+    const onPublishDataRecieved = async (newConfig) => {
+      console.log('publishURL' + newConfig.publisherUrl)
       this.setState({ loading: true })
-
       const mediaStream = await mediaDevices.getUserMedia({
         audio: true,
         video: {
@@ -39,12 +43,14 @@ export class Main extends Component {
         }
       })
 
-      const renderBroadcast = broadcastRenderer(config, mediaStream)
-
+      const renderBroadcast = broadcastRenderer(newConfig, mediaStream)
       this.setState({ renderer: renderBroadcast })
     }
     const streamAccountIdUpdated = (id) => {
       this.setState({ streamAccountId: id })
+    }
+    const tokenUpdated = (token) => {
+      this.setState({ publisherToken: token })
     }
     const streamNameUpdated = (name) => {
       this.setState({ streamName: name })
@@ -52,14 +58,29 @@ export class Main extends Component {
     const onViewerClicked = () => {
       console.log('onViewerClicked')
       getCustomConfig(
-        config,
+        staticConfig,
         this.state.streamAccountId,
         this.state.streamName,
         (newConfig) => {
           console.log('newConfig', newConfig)
           this.setState({ renderer: viewerRenderer(newConfig) })
         },
-        (error)=>{
+        (error) => {
+          console.log('showing alert dialog')
+          Alert.alert('Error !!', error)
+        }
+      )
+    }
+    const onPublishClicked = () => {
+      getPublisherCustomConfig(
+        staticConfig,
+        this.state.streamName,
+        this.state.publisherToken,
+        (newConfig) => {
+          console.log('newConfig', newConfig)
+          onPublishDataRecieved(newConfig)
+        },
+        (error) => {
           console.log('showing alert dialog')
           Alert.alert('Error !!', error)
         }
@@ -69,12 +90,6 @@ export class Main extends Component {
       <View style={styles.container}>
         <Text style={styles.title}>Nagwa Connect Millicast Mobile Demo</Text>
         <View style={styles.selections}>
-          {/* <TouchableHighlight
-            disabled = { buttonDisabled }
-            style = { styles.button }
-            onPress={ publishPressed }>
-            <Text style={ styles.buttonText }>Publish</Text>
-          </TouchableHighlight> */}
           <Text style={{ color: '#000', fontSize: 18 }}>
             Enter Stream Account Id
           </Text>
@@ -92,10 +107,22 @@ export class Main extends Component {
           <TouchableHighlight
             disabled={buttonDisabled}
             style={styles.button}
-            onPress={onViewerClicked}
-          >
+            onPress={onViewerClicked}>
             <Text style={styles.buttonText}>Viewer</Text>
           </TouchableHighlight>
+          <Text style={{ color: '#000', fontSize: 18 }}>Enter Punlisher Token</Text>
+          <TextInput
+            onChangeText={tokenUpdated}
+            value={this.state.publisherToken}
+            style={{ backgroundColor: '#fff' }}
+          />
+          <TouchableHighlight
+            disabled={buttonDisabled}
+            style={styles.button}
+            onPress={onPublishClicked}>
+            <Text style={styles.buttonText}>Publish</Text>
+          </TouchableHighlight>
+
           <Text style={{ color: '#000', fontSize: 18, display: 'none' }}>
             Loading...
           </Text>
@@ -106,16 +133,21 @@ export class Main extends Component {
 }
 
 function getCustomConfig (
-  { directorBaseURL, turnApiUrl },
+  config,
   streamAccountId,
   streamName,
   onConfigReturned,
   onConfigFailed
 ) {
-  console.log(directorBaseURL, turnApiUrl, streamAccountId, streamName)
+  console.log(
+    config.directorBaseURL,
+    config.turnApiUrl,
+    streamAccountId,
+    streamName
+  )
   return axios
     .post(
-      directorBaseURL + '/subscribe',
+      config.directorBaseURL + '/subscribe',
       {
         streamAccountId,
         streamName,
@@ -129,18 +161,61 @@ function getCustomConfig (
       }
     )
     .then((res) => {
-      console.log('wsURL', res.data.data.wsUrl + '?token=' + res.data.data.jwt)
-      onConfigReturned({
-        logger: console,
-        turnApiUrl,
-        viewerUrl: res.data.data.wsUrl + '?token=' + res.data.data.jwt,
-        viewerStreamId: streamName
-      })
+      let newConfig = config
+      newConfig['viewerUrl'] =
+        res.data.data.wsUrl + '?token=' + res.data.data.jwt
+      newConfig['viewerStreamId'] = streamName
+      console.log(newConfig)
+      onConfigReturned(newConfig)
     })
     .catch((err) => {
-      console.log(typeof err)
+      console.log(err)
       let msg = 'Something went wrong please try again'
-      try { msg = err.response.data.data.message } catch (e) {}
+      try {
+        msg = err.response.data.data.message
+      } catch (e) {}
+      onConfigFailed(msg)
+    })
+}
+function getPublisherCustomConfig (
+  config,
+  streamName,
+  publisherToken,
+  onConfigReturned,
+  onConfigFailed
+) {
+  console.log('getPublisherCustomConfig',
+    config.directorBaseURL,
+    config.turnApiUrl,
+    streamName,
+    publisherToken
+  )
+  return axios
+    .post(
+      config.directorBaseURL + '/publish',
+      {streamName},
+      {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization':'Bearer ' + publisherToken
+        }
+      }
+    )
+    .then((res) => {
+      let newConfig = config
+      newConfig['publisherUrl'] = res.data.data.wsUrl    
+      newConfig['publisherStreamId'] = streamName
+      newConfig['publisherToken'] = res.data.data.jwt
+      console.log(newConfig)
+      onConfigReturned(newConfig)
+    })
+    .catch((err) => {
+      console.log(err)
+      let msg = 'Something went wrong please try again'
+      try {
+        msg = err.response.status + err.response.data.data.message
+      } catch (e) {}
       onConfigFailed(msg)
     })
 }
